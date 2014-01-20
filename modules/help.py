9f3cd267c2a4daf9ac7f32d6a0382d62484ea994
Alessio Piccoli <alepic@geckoblu.net>
2014-01-20 15:54:28 +0100

    Help

--- modules/help.py
+++ modules/help.py
@@ -9,39 +9,40 @@ from helpBA import HelpBA
 class textbrowser(QTextBrowser):
     # reimplemented textbrowser that filters out external sources
     # future: launch web browser
-    def __init__(self, parent=None, name=None):
+    def __init__(self, parent=None):
         self.parent = parent
-        QTextBrowser.__init__(self, parent, name)
+        QTextBrowser.__init__(self, parent)
 
 
     def setSource(self, src):
-        #print "setSource:", src
+        #print("setSource:", src)
         s = str(src)
         if s[:7] == 'http://':
             launch_browser(self.parent.external_browser, s)
             return
 
-        QTextBrowser.setSource(self, src)
+        QTextBrowser.setSource(self, QUrl(src))
                 
     
                 
 
 class Help(HelpBA):
     def __init__(self, parent, filename, external_browser=None):
-        HelpBA.__init__(self, None, None)
+        HelpBA.__init__(self, None)
         #Qt.WType_TopLevel | Qt.WDestructiveClose)
         
         self.external_browser = external_browser
         self.setGeometry(100, 50, 800, 600)
-        self.setCaption("Help")
-        self.setIcon(QPixmap(xpm.kodosIcon))
-        #self.setIcon(getPixmap("kodos_icon.png", "PNG"))
+        self.setWindowTitle("Help")
+        self.setWindowIcon(QIcon(QPixmap(xpm.kodosIcon)))
         
         self.textBrowser = textbrowser(self)
         absPath = self.getHelpFile(filename)
 
         self.setCentralWidget(self.textBrowser)
         self.textBrowser.setSource(absPath)
+        
+        self.loadToolbarIcons()
         
         self.fwdAvailable = 0
         self.show()
@@ -61,9 +62,9 @@ class Help(HelpBA):
 
 
 
-    def setForwardAvailable(self, bool):
-        #print "bool: ", bool
-        self.fwdAvailable = bool
+    def setForwardAvailable(self, b):
+        #print "bool: ", b
+        self.fwdAvailable = b
 
 
     def forwardHandler(self):
@@ -75,5 +76,14 @@ class Help(HelpBA):
         f = findFile(os.path.join("help", filename))
         return f
     
+    def loadToolbarIcons(self):
+        filehomeicon = QIcon.fromTheme("go-home", QIcon(QPixmap(xpm.homeIcon)));
+        self.fileHomeAction.setIcon(filehomeicon)
+        filebackicon = QIcon.fromTheme("go-previous", QIcon(QPixmap(xpm.backIcon)));
+        self.fileBackAction.setIcon(filebackicon)
+        fileforwardicon = QIcon.fromTheme("go-next", QIcon(QPixmap(xpm.forwardIcon)));
+        self.fileForwardAction.setIcon(fileforwardicon)
+        fileexiticon = QIcon.fromTheme("application-exit", QIcon(QPixmap(xpm.exitIcon)));
+        self.fileExitAction.setIcon(fileexiticon)    
 
         

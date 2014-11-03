43cb6ad02c29ef906e42e4613b4a685feba3b358
Alessio Piccoli <alepic@geckoblu.net>
2014-11-03 14:29:37 +0100

    Disable locale

--- kang/modules/main.py
+++ kang/modules/main.py
@@ -3,14 +3,12 @@
 # pyrcc4 resources.qrc > modules/resources.py
 
 
-from PyQt4 import QtCore
 from PyQt4 import QtGui
 import argparse
 import sys
 
 from kang.gui.mainWindow import MainWindow
 from kang.modules import exceptionHandler
-from kang.modules.util import findFile
 
 
 def parse_cmdline():
@@ -23,8 +21,7 @@ def parse_cmdline():
 
     parser = argparse.ArgumentParser(description='Kang is a graphical regular expression tester.')
     parser.add_argument('filename', metavar='FILENAME', nargs='?', help='load filename on startup')
-    #parser.add_argument('-f', '--filename', help='load filename on startup')
-    parser.add_argument('-l', '--locale', type=locale, help='2-letter locale (eg. en)')
+    #parser.add_argument('-l', '--locale', type=locale, help='2-letter locale (eg. en)')
 
     return parser.parse_args()
 
@@ -35,17 +32,18 @@ def main():
 
     qApp = QtGui.QApplication(sys.argv)
 
-    if args.locale not in (None, 'en'):
-        localefile = "kang_%s.qm" % (args.locale or QtCore.QTextCodec.locale())
-        localepath = findFile("translations", localefile)
-
-        if localepath:
-            translator = QtCore.QTranslator(qApp)
-            translator.load(localepath)
-
-            qApp.installTranslator(translator)
-        else:
-            sys.stderr.write("Locale for '%s' not found. Fallback to default.\n" % args.locale)
+# TODO: Re-enable locale
+#     if args.locale not in (None, 'en'):
+#         localefile = "kang_%s.qm" % (args.locale or QtCore.QTextCodec.locale())
+#         localepath = findFile("translations", localefile)
+#
+#         if localepath:
+#             translator = QtCore.QTranslator(qApp)
+#             translator.load(localepath)
+#
+#             qApp.installTranslator(translator)
+#         else:
+#             sys.stderr.write("Locale for '%s' not found. Fallback to default.\n" % args.locale)
 
     kang = MainWindow(args.filename)
 

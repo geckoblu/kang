4b5c4d66b0d60617fecb41208d0b268793fb7f64
Alessio Piccoli <alepic@geckoblu.net>
2015-04-27 09:49:57 +0200

    Setup

--- setup.py
+++ setup.py
@@ -9,9 +9,12 @@ setup(
     version = VERSION,
     scripts = ['bin/kang'],
     packages = find_packages(exclude=['test', 'tests']),
-    package_data = {'': ['*.svg', '*.xml']},
-    #data_files = [
-    #    ('images', glob('images/kang-icon.svg'))
-    #],
+    package_data = {'': ['*.svg', '*.xml'],
+                    'kang': ['doc/*.*', 'doc/_images/*.*', 'doc/_static/*.*']
+                    },
+    data_files = [
+        ('share/pixmaps', ['kang/images/kang-icon.svg']),
+        ('share/applications', ['data/kang.desktop'])
+    ],
     zip_safe = False
 )

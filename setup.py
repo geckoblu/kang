2c6a63c7ec1a8d4b0824661fce85ee8bffbbf17f
Alessio Piccoli <alepic@geckoblu.net>
2021-06-22 10:13:37 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,79 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
+import os.path
 import sys
+
+from distutils import log
+import os
+from setuptools import  Command
+import subprocess
 
 from kang import VERSION
 
-try:
-    import PySide2
-except ImportError:
-    sys.stderr.write("""Could not locate the PySide2 module.  Please make sure that
-you have installed PySide2 for the version of Python that you are running.\n""")
-    sys.exit(1)
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+
+class integrate_desktop(Command):
+    """Subcommand to integrate kang into a desktop environment"""
+
+    description = "Integrate kang into the desktop."
+
+    user_options = []
+
+    def initialize_options(self):
+        """init options"""
+        pass
+
+    def finalize_options(self):
+        """finalize options"""
+        pass
+
+    def run(self):
+        """runner"""
+        d, __ = os.path.split(__file__)
+        d = os.path.join(d, 'data')
+
+        # sudo xdg-mime install --mode system data/kang-mimetype.xml
+        log.info('installing mime type')
+        mimetype_filename = os.path.join(d, 'kang-mimetype.xml')
+        subprocess.call(['xdg-mime', 'install', mimetype_filename])
+
+        # sudo xdg-icon-resource install --mode system --size 48 application-x-kang.png
+        log.info('installing file icon')
+        mimeicon_filename = os.path.join(d, 'application-x-kang.png')
+        subprocess.call(['xdg-icon-resource', 'install', '--size', '48', mimeicon_filename])
+        
+        # sudo xdg-mime default kang.desktop application/x-kang
+        log.info('link mime type to kang')
+        subprocess.call(['xdg-mime', 'default', 'kang.desktop', 'application/x-kang'])
 
 cmdclass = {}
-try:
-    from setuptools_extensions import integrate_desktop
-    cmdclass['integrate_desktop'] = integrate_desktop
-except ImportError:
-    pass
-
-
+cmdclass['integrate_desktop'] = integrate_desktop
+        
 setup(
     name='kang',
     version=VERSION,
+    url='https://github.com/geckoblu/kang',
+    author='Alessio Piccoli',
+    author_email='alepic@geckoblu.net',
+    description="The Python Regular Expression Editor",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="GPL v2",
+    
     scripts=['bin/kang'],
     packages=find_packages(exclude=['test', 'tests']),
-    package_data={'': ['*.svg', '*.xml'],
-                    'kang': ['doc/*.*', 'doc/_images/*.*', 'doc/_static/*.*']
-                  },
+    package_data={'': ['*.svg'] },
     data_files=[
         ('share/pixmaps', ['kang/images/kang-icon.svg']),
         ('share/applications', ['data/kang.desktop'])
     ],
     zip_safe=False,
-    cmdclass=cmdclass,
-
-    url='http://www.geckoblu.net/kang',
-    author='Alessio Piccoli',
-    author_email='alepic@geckoblu.net'
+    cmdclass=cmdclass,    
 )
